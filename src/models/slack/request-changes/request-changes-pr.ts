ca22e09652ca06d09e486e4967e577ed6b608dfa
Ethan Painter <ethantpainter@gmail.com>
2019-01-03 14:58:45 -0500

    Adds request-changes model and construction of it

--- src/models/slack/request-changes/request-changes-pr.ts
+++ src/models/slack/request-changes/request-changes-pr.ts
@@ -4,8 +4,8 @@ import { Base } from "../../base";
  * @Author: Ethan T Painter
  * Model for when changes are requested in a PR
  *
- * owner                    - User who opened the PR
- * user_requesting_changes  - User who requested changes for the PR
+ * owner                    - GitHub User who opened the PR
+ * user_requesting_changes  - GitHub User who requested changes for the PR
  */
 export class RequestChangesPR extends Base {
   owner: string;
--- src/slack/message/construct/formatting/base.ts
+++ src/slack/message/construct/formatting/base.ts
@@ -34,6 +34,10 @@ export class Base {
     }
   }
 
+  // Functions below are very repetitive
+  // Can probably find a much better way of
+  // finding a result without so much code
+
   /**
    * Not enough information about which team the user is in, so search all (for now)
    * @author Ethan T Painter
@@ -65,7 +69,7 @@ export class Base {
         const users: any = selectedTeamGroup.Users;
         // Check if githubUser is a Lead for the group
         // There may exist multiple users in Lead json
-        const leadUsers: any = users.Lead;
+        const leadUsers: any = users.Leads;
         const leadKeys: string[] = Object.keys(leadUsers);
         let leadCounter: number = 0;
         // Loop through lead keys for matching GitHub User
@@ -137,7 +141,7 @@ export class Base {
         } else {
           // Check if githubUser is a Lead for the group
           // There may exist multiple users in Lead json
-          const leadUsers: any = users.Lead;
+          const leadUsers: any = users.Leads;
           const leadKeys: string[] = Object.keys(leadUsers);
           let leadCounter: number = 0;
           // Loop through lead keys for matching GitHub User
@@ -172,5 +176,113 @@ export class Base {
     // Looped through all teams and couldn't find github group for user
     // Return empty string for no group found
     return "";
+  }
+
+  /**
+   * @author Ethan T Painter
+   * @description Using the GitHub username, find the
+   *       group and retrieve the leads for that group
+   * @param githubUser GitHub username in JSON file
+   * @returns array of Slack users
+   */
+  getLeads(githubUser: string): string[] {
+    const jsonFile: any = json;
+    // Navigates through JSON file from top to down (DevTeam -> QaTeam -> ProdTeam)
+    const teams: any = jsonFile.Teams;
+    const allTeamKeys: string[] = Object.keys(teams);
+    // If no teams present, return error
+    if (allTeamKeys.length === 0) {
+      throw new Error("No Team found in JSON file");
+    }
+
+    // Otherwise loop through teams (DevTeam, QaTeam, ProdTeam)
+    let teamCounter: number = 0;
+    while (teamCounter < allTeamKeys.length) {
+      // Get selectedTeam (DevTeam), and get selectedTeamGroup (DevTeam1)
+      const selectedTeam: string = allTeamKeys[teamCounter];
+      const selectedTeamGroup: any = teams[selectedTeam];
+      const teamGroupKeys: string[] = Object.keys(selectedTeamGroup);
+      let selectedTeamTypeCounter: number = 0;
+      // Loop through team groups (DevTeam1, DevTeam2, etc.)
+      while (selectedTeamTypeCounter < teamGroupKeys.length) {
+        // Retrieve usersfrom JSON
+        const users: any = selectedTeamGroup.Users;
+        // Check if githubUser is a Lead for the group
+        // There may exist multiple users in Lead json
+        const leadUsers: any = users.Leads;
+        const leadKeys: string[] = Object.keys(leadUsers);
+        let leadCounter: number = 0;
+        // Loop through lead keys for matching GitHub User
+        while (leadCounter < leadKeys.length) {
+          // Check if key matches GitHub user
+          if (leadKeys[leadCounter] === githubUser) {
+            // Return all leads
+            return Object.values(users.Leads);
+          }
+          leadCounter++;
+        }
+        // If GitHub user not found in lead or member groups
+        // The user must be in a different group
+        selectedTeamTypeCounter++;
+      }
+      // User not found in General Team (DevTeam), look at antoher team
+      teamCounter++;
+    }
+
+    return [];
+  }
+
+  /**
+   * @author Ethan T Painter
+   * @description Using the GitHub username, find the
+   *       group and retrieve the leads for that group
+   * @param githubUser GitHub username in JSON file
+   * @returns array of slack users
+   */
+  getMembers(githubUser: string): string[] {
+    const jsonFile: any = json;
+    // Navigates through JSON file from top to down (DevTeam -> QaTeam -> ProdTeam)
+    const teams: any = jsonFile.Teams;
+    const allTeamKeys: string[] = Object.keys(teams);
+    // If no teams present, return error
+    if (allTeamKeys.length === 0) {
+      throw new Error("No Team found in JSON file");
+    }
+
+    // Otherwise loop through teams (DevTeam, QaTeam, ProdTeam)
+    let teamCounter: number = 0;
+    while (teamCounter < allTeamKeys.length) {
+      // Get selectedTeam (DevTeam), and get selectedTeamGroup (DevTeam1)
+      const selectedTeam: string = allTeamKeys[teamCounter];
+      const selectedTeamGroup: any = teams[selectedTeam];
+      const teamGroupKeys: string[] = Object.keys(selectedTeamGroup);
+      let selectedTeamTypeCounter: number = 0;
+      // Loop through team groups (DevTeam1, DevTeam2, etc.)
+      while (selectedTeamTypeCounter < teamGroupKeys.length) {
+        // Retrieve usersfrom JSON
+        const users: any = selectedTeamGroup.Users;
+
+        // Check member group
+        const memberUsers: any = users.Members;
+        const memberKeys: string[] = Object.keys(memberUsers);
+        let memberCounter: number = 0;
+        // Loop through member keys for matching GitHub User
+        while (memberCounter < memberKeys.length) {
+          // Check if key matches GitHub user
+          if (memberKeys[memberCounter] === githubUser) {
+            // Return all members
+            return Object.values(memberUsers);
+          }
+          memberCounter++;
+        }
+
+        // If GitHub user not found in lead or member groups
+        // The user must be in a different group
+        selectedTeamTypeCounter++;
+      }
+      // User not found in General Team (DevTeam), look at antoher team
+      teamCounter++;
+    }
+    return [];
   }
 }
