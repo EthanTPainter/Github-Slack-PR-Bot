8ebe8a28cfc8849643856533aeab2acb69529d46
Ethan Painter <ethantpainter@gmail.com>
2019-04-27 20:13:19 -0400

    renames disabling GitHub notifications option to make it less confusing

--- src/models/json/team-options.ts
+++ src/models/json/team-options.ts
@@ -13,7 +13,7 @@ export class TeamOptions {
   Num_Required_Member_Approvals: number;  // Number of required member approvals
   Num_Required_Lead_Approvals: number;    // Number of required lead approvals
   Disable_Dynamo: boolean;                // Disable Dynamo use
-  Disable_Slack: boolean;                 // Disable Slack alerts
+  Disable_GitHub_Alerts: boolean;         // Disable GitHub to Slack alerts
   Member_Before_Lead: boolean;      // If true, When adding new PR to dynamo queue,
                                     // prioritize adding to member queues before lead queues
                                     // Adds to lead queues after fully member approved
