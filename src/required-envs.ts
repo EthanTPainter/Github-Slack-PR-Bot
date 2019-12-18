6b9410f93e0420605060bc9191397ea14f9da435
Ethan Painter <ethantpainter@gmail.com>
2019-12-18 08:32:15 -0500

    Remove serverless file and change required envs

--- src/required-envs.ts
+++ src/required-envs.ts
@@ -6,21 +6,15 @@ const { bool, str } = envalid;
 
 export class EnvModel {
   LOG_LEVEL: string;
-  SLS_DEBUG: string;
-  DISABLE_XRAY: boolean;
-  S3_BUCKET_NAME: string;
   GITHUB_OAUTH2_TOKEN: string;
   SLACK_API_URI: string;
   DYNAMO_TABLE_NAME: string;
   DYNAMO_API_VERSION: string;
   DYNAMO_REGION: string;
-  SNS_ARN: string;
-  SNS_API_VERSION: string;
-  SQS_ARN: string;
+  SQS_URL: string;
   SQS_API_VERSION: string;
   INTEGRATION_TEST_DYNAMO_TABLE_NAME: string;
   SLACK_BOT_TOKEN: string;
-  Mighty_Minks_SLACK_CHANNEL_NAME: string;
   /* Add Your Team Name following the format:
   * [TEAM NAME]_SLACK_CHANNEL_NAME: string;
   */
@@ -28,21 +22,15 @@ export class EnvModel {
 
 export const requiredEnvs = envalid.cleanEnv<EnvModel>(process.env, {
   LOG_LEVEL: str({ default: "debug" }),
-  SLS_DEBUG: str({ default: "*" }),
-  DISABLE_XRAY: bool({ default: false }),
-  S3_BUCKET_NAME: str({ default: "pr-bot-s3-bucket" }),
   GITHUB_OAUTH2_TOKEN: str(),
   SLACK_API_URI: str({ default: "https://slack.com/api" }),
   DYNAMO_TABLE_NAME: str({ default: "pr-bot-table" }),
   DYNAMO_API_VERSION: str({ default: "2012-08-10"}),
   DYNAMO_REGION: str({ default: "us-east-1" }),
-  SNS_ARN: str(),
-  SNS_API_VERSION: str({ default: "2010-03-31" }),
-  SQS_ARN: str(),
+  SQS_URL: str(),
   SQS_API_VERSION: str({ default: "2012-11-05" }),
   INTEGRATION_TEST_DYNAMO_TABLE_NAME: str({ default: "pr-bot-test-table" }),
   SLACK_BOT_TOKEN: str(),
-  Mighty_Minks_SLACK_CHANNEL_NAME: str(),
   /* Add Your Team Name following the format:
   * [TEAM NAME]_SLACK_CHANNEL_NAME: str(),
   */
