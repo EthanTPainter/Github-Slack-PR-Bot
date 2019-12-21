ba349653a150df56ca5a5166070469ffd251e1c8
Ethan Painter <ethantpainter@gmail.com>
2019-12-20 21:34:00 -0500

    Fixes json examples

--- src/json/src/json.ts
+++ src/json/src/json.ts
@@ -1,100 +1,110 @@
-export const json = {
-  Departments: {
-    Dev: {
-      Mighty_Minks: {
-        Options: {
-          Avoid_Slack_Channel_Comment_Alerts_Time_Window: 5,
-          Check_Mark_Text: ":heavy_check_mark:",
-          X_Mark_Text: ":X:",
-          Queue_Include_Created_Time: true,
-          Queue_Include_Updated_Time: true,
-          Queue_Include_Approval_Names: true,
-          Queue_Include_Req_Changes_Names: true,
-          Queue_Include_Owner: true,
-          Queue_Include_New_Line: false,
-          Num_Required_Lead_Approvals: 1,
-          Num_Required_Member_Approvals: 2,
-          Member_Before_Lead: true,
-          Disable_GitHub_Alerts: false,
-        },
-        Slack_Group: {
-          Slack_Name: "Minks",
-          Slack_Id: "<!subteam^SE2FY1HHA|@minks>",
-        },
-        Users: {
-          Leads: {
-            gwely: {
-              Slack_Name: "Andrew",
-              Slack_Id: "<@UBP6A5PUL>",
+import { JSONConfig } from "../../../src/models/json";
+
+export const json: JSONConfig = {
+	Departments: {
+		Dev: {
+			SCRIPTER: {
+				Options: {
+					Avoid_Comment_Alerts: 5,
+					Check_Mark_Text: ":heavy_check_mark:",
+					X_Mark_Text: ":X:",
+					Queue_Include_Created_Time: true,
+					Queue_Include_Updated_Time: true,
+					Queue_Include_Approval_Names: true,
+					Queue_Include_Req_Changes_Names: true,
+					Queue_Include_Owner: true,
+					Queue_Include_New_Line: true,
+					Num_Required_Lead_Approvals: 0,
+					Num_Required_Member_Approvals: 2,
+					Member_Before_Lead: false,
+					Disable_GitHub_Alerts: false,
+				},
+				Slack_Group: {
+					Slack_Name: "Scripter Dev Team",
+					Slack_Id: "<!subteam^SREAS7JCA|@scripter-dev-team>",
+				},
+				Users: {
+					Leads: {
+						evenflow58: {
+							Slack_Name: "Evan",
+							Slack_Id: "<@UH29FQUMN>",
             },
-          },
-          Members: {
-            DillonSykes: {
-              Slack_Name: "Dillon",
-              Slack_Id: "<@UBNT2JKRQ>",
+            ofolis: {
+							Slack_Name: "Josh",
+							Slack_Id: "<@U5CSJDH9Q>",
+						},
+					},
+					Members: {
+						dlarner3194: {
+							Slack_Name: "Daniel",
+							Slack_Id: "<@UBM5N8EGL>",
+						},
+						EthanTPainter: {
+							Slack_Name: "Ethan",
+							Slack_Id: "<@UB5EWEB3M>",
+						},
+						helwigmegan: {
+							Slack_Name: "Megan",
+							Slack_Id: "<@UH2DL9V7X>",
             },
-            dlarner3194: {
-              Slack_Name: "Daniel",
-              Slack_Id: "<@UBM5N8EGL>",
+            MarkFreedman: {
+              Slack_Name: "Mark",
+              Slack_Id: "<@UPU8KMUQH>",
             },
-            joshuajharris: {
-              Slack_Name: "Joshua",
-              Slack_Id: "<@U78G0U2HZ>",
+					},
+				},
+			},
+			CONNECT: {
+				Options: {
+					Avoid_Comment_Alerts: 5,
+					Check_Mark_Text: ":heavy_check_mark:",
+					X_Mark_Text: ":X:",
+					Queue_Include_Created_Time: true,
+					Queue_Include_Updated_Time: true,
+					Queue_Include_Approval_Names: true,
+					Queue_Include_Req_Changes_Names: true,
+					Queue_Include_Owner: true,
+					Queue_Include_New_Line: false,
+					Num_Required_Lead_Approvals: 1,
+					Num_Required_Member_Approvals: 1,
+					Member_Before_Lead: true,
+					Disable_GitHub_Alerts: false,
+				},
+				Slack_Group: {
+					Slack_Name: "Connect Team",
+					Slack_Id: "<!subteam^SRFMK0W8K|@connect-team>",
+				},
+				Users: {
+					Leads: {
+						dinkzilla: {
+							Slack_Name: "Matt",
+							Slack_Id: "<@UBDE41BK6>",
             },
-            EthanTPainter: {
-              Slack_Name: "Ethan",
-              Slack_Id: "<@UB5EWEB3M>",
+            "bgidwani-dgdean": {
+              Slack_Name: "Bharat",
+              Slack_Id: "<@UPFQ8CMG8>",
             },
-          },
-        },
-      },
-      Dev_Team_2: {
-        Options: {
-          Avoid_Slack_Channel_Comment_Alerts_Time_Window: 5,
-          Check_Mark_Text: ":heavy_check_mark:",
-          X_Mark_Text: ":X:",
-          Queue_Include_Created_Time: true,
-          Queue_Include_Updated_Time: true,
-          Queue_Include_Approval_Names: true,
-          Queue_Include_Req_Changes_Names: true,
-          Queue_Include_Owner: true,
-          Queue_Include_New_Line: false,
-          Num_Required_Lead_Approvals: 1,
-          Num_Required_Member_Approvals: 1,
-          Member_Before_Lead: true,
-          Disable_GitHub_Alerts: false,
-        },
-        Slack_Group: {
-          Slack_Name: "Bite Sized Turkeys",
-          Slack_Id: "<!subteam^SEMDYC7FC|@bitesizedturkeys>",
-        },
-        Users: {
-          Leads: {
-            dinkzilla: {
-              Slack_Name: "MattyDinks",
-              Slack_Id: "<@UBDE41BK6>",
+					},
+					Members: {
+						DillonSykes: {
+							Slack_Name: "Dillon",
+							Slack_Id: "<@UBNT2JKRQ>",
+						},
+						crewsha: {
+							Slack_Name: "Harrison",
+							Slack_Id: "<@UCZGFLHS8>",
             },
-          },
-          Members: {
-            crewsha: {
-              Slack_Name: "Harrison",
-              Slack_Id: "<@UCZGFLHS8>",
+            stam999: {
+              Slack_Name: "Stan",
+              Slack_Id: "<@UQ268G3DK>",
             },
-            turnquistnt: {
-              Slack_Name: "Nicholas",
-              Slack_Id: "<@U6G80SL4U>",
+            myakubdgdean: {
+              Slack_Name: "Mike",
+              Slack_Id: "<@UL74V8GTC>",
             },
-            helwigmegan: {
-              Slack_Name: "Megan",
-              Slack_Id: "<@UH2DL9V7X>",
-            },
-            evenflow58: {
-              Slack_Name: "Evan",
-              Slack_Id: "<@UH29FQUMN>",
-            },
-          },
-        },
-      },
-    },
-  },
+					},
+				},
+			},
+		},
+	},
 };
--- src/json/src/raw-example.ts
+++ src/json/src/raw-example.ts
@@ -1,100 +1,102 @@
-export const json = {
-  Department: {
-    Dev: {
-      Dev_Team_1: {
-        Options: {
-          Avoid_Slack_Channel_Comment_Alerts_Time_Window: 5,
-          Check_Mark_Text: ":heavy_check_mark:",
-          X_Mark_Text: ":X:",
-          Queue_Include_Created_Time: true,
-          Queue_Include_Updated_Time: true,
-          Queue_Include_Approval_Names: true,
-          Queue_Include_Req_Changes_Names: true,
-          Queue_Include_Owner: true,
-          Queue_Include_New_Line: false,
-          Num_Required_Lead_Approvals: 1,
-          Num_Required_Member_Approvals: 1,
-          Member_Before_Lead: true,
-          Disable_GitHub_Alerts: false,
-        },
-        Slack_Group: {
-          Slack_Name: "TeamName1",
-          Slack_ID: "<!subteam^ID|handle>",
-        },
-        Users: {
-          Leads: {
-            GitHub_Username_1: {
-              Slack_Name: "FirstName LastName",
-              Slack_Id: "<@ID_0>",
-            },
-          },
-          Members: {
-            GitHub_Username_2: {
-              Slack_Name: "FirstName LastName",
-              Slack_Id: "<@ID_1>",
-            },
-            GitHub_Username_3: {
-              Slack_Name: "FirstName LastName",
-              Slack_Id: "<@ID_2>",
-            },
-            GitHub_Username_4: {
-              Slack_Name: "FirstName LastName",
-              Slack_Id: "<@ID_3>",
-            },
-            GitHub_Username_5: {
-              Slack_Name: "FirstName LastName",
-              Slack_Id: "<@ID_4>",
-            },
-          },
-        },
-      },
-      Dev_Team_2: {
-        Options: {
-          Avoid_Slack_Channel_Comment_Alerts_Time_Window: 5,
-          Check_Mark_Text: ":happy:",
-          X_Mark_Text: ":angry:",
-          Queue_Include_Created_Time: true,
-          Queue_Include_Updated_Time: true,
-          Queue_Include_Approval_Names: true,
-          Queue_Include_Req_Changes_Names: true,
-          Queue_Include_Owner: true,
-          Queue_Include_New_Line: false,
-          Num_Required_Lead_Approvals: 1,
-          Num_Required_Member_Approvals: 1,
-          Member_Before_Lead: true,
-          Disable_GitHub_Alerts: false,
-        },
-        Slack_Group: {
-          Slack_Name: "TeamName2",
-          Slack_Id: "<!subteam^ID|handle>",
-        },
-        Users: {
-          Leads: {
-            GitHub_Username_1: {
-              Slack_Name: "FirstName LastName",
-              Slack_Id: "<@ID_0>",
-            },
-          },
-          Members: {
-            GitHub_Username_2: {
-              Slack_Name: "FirstName LastName",
-              Slack_Id: "<@ID_1>",
-            },
-            GitHub_Username_3: {
-              Slack_Name: "FirstName LastName",
-              Slack_Id: "<@ID_2>",
-            },
-            GitHub_Username_4: {
-              Slack_Name: "FirstName LastName",
-              Slack_Id: "<@ID_3>",
-            },
-            GitHub_Username_5: {
-              Slack_Name: "FirstName LastName",
-              Slack_Id: "<@ID_4>",
-            },
-          },
-        },
-      },
-    },
-  },
+import { JSONConfig } from "../../../src/models";
+
+export const json: JSONConfig = {
+	Departments: {
+		Dev: {
+			Dev_Team_1: {
+				Options: {
+					Avoid_Comment_Alerts: 5,
+					Check_Mark_Text: ":heavy_check_mark:",
+					X_Mark_Text: ":X:",
+					Queue_Include_Created_Time: true,
+					Queue_Include_Updated_Time: true,
+					Queue_Include_Approval_Names: true,
+					Queue_Include_Req_Changes_Names: true,
+					Queue_Include_Owner: true,
+					Queue_Include_New_Line: false,
+					Num_Required_Lead_Approvals: 1,
+					Num_Required_Member_Approvals: 1,
+					Member_Before_Lead: true,
+					Disable_GitHub_Alerts: false,
+				},
+				Slack_Group: {
+					Slack_Name: "TeamName1",
+					Slack_Id: "<!subteam^ID|handle>",
+				},
+				Users: {
+					Leads: {
+						GitHub_Username_1: {
+							Slack_Name: "FirstName LastName",
+							Slack_Id: "<@ID_0>",
+						},
+					},
+					Members: {
+						GitHub_Username_2: {
+							Slack_Name: "FirstName LastName",
+							Slack_Id: "<@ID_1>",
+						},
+						GitHub_Username_3: {
+							Slack_Name: "FirstName LastName",
+							Slack_Id: "<@ID_2>",
+						},
+						GitHub_Username_4: {
+							Slack_Name: "FirstName LastName",
+							Slack_Id: "<@ID_3>",
+						},
+						GitHub_Username_5: {
+							Slack_Name: "FirstName LastName",
+							Slack_Id: "<@ID_4>",
+						},
+					},
+				},
+			},
+			Dev_Team_2: {
+				Options: {
+					Avoid_Comment_Alerts: 5,
+					Check_Mark_Text: ":happy:",
+					X_Mark_Text: ":angry:",
+					Queue_Include_Created_Time: true,
+					Queue_Include_Updated_Time: true,
+					Queue_Include_Approval_Names: true,
+					Queue_Include_Req_Changes_Names: true,
+					Queue_Include_Owner: true,
+					Queue_Include_New_Line: false,
+					Num_Required_Lead_Approvals: 1,
+					Num_Required_Member_Approvals: 1,
+					Member_Before_Lead: true,
+					Disable_GitHub_Alerts: false,
+				},
+				Slack_Group: {
+					Slack_Name: "TeamName2",
+					Slack_Id: "<!subteam^ID|handle>",
+				},
+				Users: {
+					Leads: {
+						GitHub_Username_1: {
+							Slack_Name: "FirstName LastName",
+							Slack_Id: "<@ID_0>",
+						},
+					},
+					Members: {
+						GitHub_Username_2: {
+							Slack_Name: "FirstName LastName",
+							Slack_Id: "<@ID_1>",
+						},
+						GitHub_Username_3: {
+							Slack_Name: "FirstName LastName",
+							Slack_Id: "<@ID_2>",
+						},
+						GitHub_Username_4: {
+							Slack_Name: "FirstName LastName",
+							Slack_Id: "<@ID_3>",
+						},
+						GitHub_Username_5: {
+							Slack_Name: "FirstName LastName",
+							Slack_Id: "<@ID_4>",
+						},
+					},
+				},
+			},
+		},
+	},
 };
